9abe6de883b0835a96c261ec51b8e51725e55e2d
aledevtg <alejandrotrinidad28@hotmail.com>
2022-06-06 23:19:03 -0500

    Interfaz calculadora

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Calculator from './calculator';
 import './App.css';
 
 function App() {
   return (
-    <h1 id="titule"> Calculator by Chowder</h1>
+    <div>
+    <h1 id="titule"> Calculator by Chowder </h1>
+    <Calculator />
+    </div>
+    
   );
 }
 
---
+++ src/calculator.js
@@ -0,0 +1,12 @@
+import './calculator.css';
+
+
+function Calculator(){
+
+    return(
+        <div class="container"> 
+        
+        </div>
+    );
+}
+export default Calculator;
