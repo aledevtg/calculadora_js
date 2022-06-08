64cfe75582da607eab5ffb2b19f035079ba38d2b
aledevtg <alejandrotrinidad28@hotmail.com>
2022-06-07 23:52:55 -0500

    cambio de funcion a constante

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,35 +1,37 @@
 import './styles/calculator.css';
 
 
-function Calculator(){
+const Calculator = (props) => {
 
     return(
         <div class="container"> 
             <div class="comp-1"> 
                 <button id="ce-1"> CE </button>
                 <div class="screen">
-                    <h3 class="res">0</h3>
+                    <h3 class="res">{props.res}</h3>
                 </div>
             </div>
-            <div class="btn-1-op"> 
-                <button class="type-1-sim">7</button>
-                <button class="type-1-sim">8</button>
-                <button class="type-1-sim">9</button>
-                <button class="type-1-sim" id="color-btn">/</button>
-                <button class="type-1-sim">4</button>
-                <button class="type-1-sim">5</button>
-                <button class="type-1-sim">6</button>
-                <button class="type-1-sim" id="color-btn">*</button>
-                <button class="type-1-sim">1</button>
-                <button class="type-1-sim">2</button>
-                <button class="type-1-sim">3</button>
-                <button class="type-1-sim" id="color-btn">-</button>
-                <button class="type-1-sim">0</button>
-                <button class="type-1-sim">.</button>
+            <div class="btn-1-op" id="num_sim"> 
+                <button class="type-1-sim" value="7">7</button>
+                <button class="type-1-sim" value="8">8</button>
+                <button class="type-1-sim" value="9">9</button>
+                <button class="type-1-sim" id="color-btn" value="/">/</button>
+                <button class="type-1-sim" value="4">4</button>
+                <button class="type-1-sim" value="5">5</button>
+                <button class="type-1-sim" value="6">6</button>
+                <button class="type-1-sim" id="color-btn" value="*">*</button>
+                <button class="type-1-sim" value="1">1</button>
+                <button class="type-1-sim" value="2">2</button>
+                <button class="type-1-sim" value="3">3</button>
+                <button class="type-1-sim" id="color-btn" value="-">-</button>
+                <button class="type-1-sim" value="0">0</button>
+                <button class="type-1-sim" value=".">.</button>
                 <button class="type-1-sim" id="color-btn">=</button>
-                <button class="type-1-sim" id="color-btn">+</button>
+                <button class="type-1-sim" id="color-btn" value="+">+</button>
                 </div>
         </div>
     );
 }
+
+
 export default Calculator;
